83611dd859fd90236f49f63c95026e026e936d88
nguyen.quoc.hoa <SUN-ASTERISK\nguyen.quoc.hoa@b122556-pc.sun-asterisk.com>
2021-07-15 16:44:15 +0700

    finaly 3

--- app/controllers/course_users_controller.rb
+++ app/controllers/course_users_controller.rb
@@ -1,18 +1,22 @@
 class CourseUsersController < ApplicationController
-  before_action :load_course_user, only: [:show]
+  before_action :load_course_user, only: :show
 
   def index
-    @course_users = current_user.includes(:course).page(params[:page]).per(Settings.page.per)
+    @course_users = current_user.course_users.includes(:course)
+                                .page(params[:page]).per(Settings.page.per)
   end
 
   def show
     @course = @course_user.course
+    @user_course_subjects = @course_user.user_course_subjects
+                                        .includes course_subject: [:subject]
+    flash.now[:danger] = t(:not_have_subject) unless @user_course_subjects.any?
   end
 
   private
 
   def load_course_user
-    @course_user = current_user.course_users.find_by id: params[:id]
+    @course_user = current_user.course_users.find_by course_id: params[:course_id], user_id: params[:user_id]
     return if @course_user
 
     flash[:danger] = t :flash_danger_c_course_users
--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,2 +1,6 @@
 class UserTask < ApplicationRecord
+  belongs_to :task
+  belongs_to :user_course_subject, autosave: true
+
+  enum status: {in_process: 0, done: 1, pending: 2, closed: 3}
 end
